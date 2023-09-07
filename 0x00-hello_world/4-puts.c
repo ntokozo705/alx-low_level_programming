3453cc2904d8d87b4bef6880c54abe5bda9b03e4
ntokozo705 <ntokozo705@gmail.com>
2023-09-07 07:35:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
  *  main- func for puts
- *puts function call
- *return (0)
- * /
+ *
+ * return: zero
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+ 	 puts("\"Programming is like building a multilingual puzzle");
+  	return (0);
 
 }
