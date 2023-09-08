ad542ad386a36cd96e340a6cf00eeae1392038ee
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 14:18:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- print numbers
+ *
+ * Return: 0
+ */
+
+int main(viod)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+	return (0);
+}
