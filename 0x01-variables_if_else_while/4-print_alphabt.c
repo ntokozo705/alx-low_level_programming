656ee2d93588cc23a0369a77961198fe291cd999
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 13:58:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- the putchar func for letters
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'q' && l != 'e')
+		{
+			putchar(l);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
