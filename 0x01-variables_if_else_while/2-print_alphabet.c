478b9b9f6691a9c43f2e9c6d21a52a2331db5013
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 13:28:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char L;
+	for (L = 'a';L <= 'z';L++)
+	{
+		putchar(L);
+		putchar("\n")
+		return (0);
+	}
+}
