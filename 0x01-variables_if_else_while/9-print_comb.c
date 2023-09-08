76afd16afee7630cbf78ff3f2574f6b843c08401
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 14:58:13 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main- print all possible combination
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <10; n++)
+	{
+		if (n == 9)
+		{
+			putchar(n);
+		}
+		else
+		{
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
