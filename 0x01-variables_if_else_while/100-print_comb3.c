cde7fdee1224cd7d7382e3ddef5989801aa57dd1
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 18:28:47 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,16 @@ int main(void)
 	{
 		for (y = x + 1; y < 10; y++)
 		{
-			putchar((x) + '0');
-			putchar(y + '0');
+			putchar((x % 10) + '0');
+			putchar(( y % 10) + '0');
 			if (x != 8 && y != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
+				
 		}
 	}
 	putchar('\n');
-
-
 	return (0);
 }
