b326921d907ae4cdc23d426c5e2454ab99321a4d
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 17:44:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,13 +12,9 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		if (n == 9)
+		putchar(n + '0');
+		if (n != 9)
 		{
-			putchar(n + '0');
-		}
-		else
-		{
-			putchar(n + '0');
 			putchar(',');
 			putchar(' ');
 		}
