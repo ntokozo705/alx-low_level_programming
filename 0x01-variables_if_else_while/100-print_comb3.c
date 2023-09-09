4c63683fed04d538272c05b1d032bc8e18dddd3d
ntokozo705 <ntokozo705@gmail.com>
2023-09-08 18:03:24 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- double digit putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			putchar(x % 10 + '0');
+			putchar(y % 10 + '0');
+			if (x != 8 && y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+
+	return (0);
