8078963267a9a25ccb83a07862eedc3c43c13b00
ntokozo705 <ntokozo705@gmail.com>
2023-09-12 13:58:48 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - the main add function
+ * Description: returns sum of two int 
+ *
+ * Return: 1 success
+ */
+
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
