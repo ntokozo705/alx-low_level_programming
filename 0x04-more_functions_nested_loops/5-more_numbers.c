02116b8b6fd5b670c261301262f689e969cbe252
ntokozo705 <ntokozo705@gmail.com>
2023-09-14 15:06:48 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@
 
 void more_numbers(void)
 {
-	int n ,m;
+	int n, m;
 
-	for ( n = 0; n <= 10; n++)
+	for (n = 0; n <= 10; n++)
 	{
-		for ( m = 0; m <=14; m++)
+		for (m = 0; m <= 14; m++)
 		{
 
 			if (m >= 10)
