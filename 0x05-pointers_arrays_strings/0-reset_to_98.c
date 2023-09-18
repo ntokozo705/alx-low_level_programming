693c63267de38e20931be14e75dccaed1851dff9
ntokozo705 <ntokozo705@gmail.com>
2023-09-18 06:19:55 -0700

    ./0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - the function that calls ther pointer
+ * @n: the point variable
+ * updates the value of the pointer to 98
+ * Return: 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+poiters and arrays reame file
