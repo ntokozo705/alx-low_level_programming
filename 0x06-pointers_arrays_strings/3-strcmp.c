dac78c9f2fd936018653dda183a789faaacc3f42
ntokozo705 <ntokozo705@gmail.com>
2023-09-21 08:35:09 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,14 +5,14 @@
  * @s1: The first string
  * @s2: Second string
  *
- * Return: s1 & s2
+ * Return: s1-s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while ((s1[i] != '\0') && (s2[i] = '\0'))
+	while ((s1[i] != '\0') && (s2[i] != '\0'))
 	{
 		if (s1[i] != s2[i])
 		{
