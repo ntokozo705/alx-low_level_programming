1dd8a868dd687c44f5cf22e762b5445e52c39d84
ntokozo705 <ntokozo705@gmail.com>
2023-09-27 11:42:41 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: the first string
+ * @s2: Second string
+ *
+ * Return: 1 if success 0 if different
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
