3c3b0aed7dbe2be91d600b227e195ea5981e12c3
ntokozo <ntokozo705@gmail.com>
2023-10-19 16:44:40 -0700

    myfirst

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,12 +8,12 @@
 void myfirst(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before main
+ * myfirst - prints a sentence before main
  *
  */
 
 
-void first(void)
+void myfirst(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
