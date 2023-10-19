f402bf7675cab96fccaf904d7312678af857f842
ntokozo <ntokozo705@gmail.com>
2023-10-19 16:08:32 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t size;
+	int size = 0;
 
 	while (h)
 	{
@@ -19,7 +19,9 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		size++;
 	}
