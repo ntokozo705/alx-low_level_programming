4b51266999d7d977c7bce67f7ee256997385fc5c
ntokozo <ntokozo705@gmail.com>
2023-10-24 16:45:55 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -13,7 +13,7 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *fast = head;
 
 	if (!head)
-		return(NULL);
+		return (NULL);
 	while (slow && fast && fast->next)
 	{
 		fast = fast->next->next;
